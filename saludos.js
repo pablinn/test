d3ce8ea06b0f5e2d0fc9db42ecd3f463364264c3
pablinn <penalve.sergio@yahoo.com.ar>
2023-08-21 18:52:07 -0300

    primer commit

---
+++ saludos.js
@@ -0,0 +1,11 @@
+function saludo(nombre){
+ return console.log(`Este es mi nombre,${nombre}`)
+}
+//console.log("Hola mundoddddd");
+//saludo("Pedro")
+
+function holamundo(){
+    return console.log('Hola Mundo')
+}
+
+module.exports = {saludo,holamundo}
